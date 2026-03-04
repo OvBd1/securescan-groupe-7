651331ca8f68dd6a598271385af189e0f7794c7f
OvBd <y.bontrond@ecole-ipssi.net>
2026-03-04 14:17:45 +0100

    Ajout route Owasp

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './src/routes/auth.routes.js';
+import projetsRoutes from './src/routes/projets.routes.js';
+import owaspRoutes from './src/routes/owasp.routes.js';
 
 const app = express();
 app.use(cors({
@@ -17,12 +19,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.send('Serveur démarré'));
 
 app.use('/auth', authRoutes);
-//app.use('/project', _router);
+app.use('/project', projetsRoutes);
+app.use('/owasp', owaspRoutes);
 //app.use('/scan', _router);
 
 //app.get('*', (req, res) => res.status(404).send('Not Found'));
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Internal Server Error');
 });
